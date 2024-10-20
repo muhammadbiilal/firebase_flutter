7a2f9d8b3997128a18fa432191cd471209f71818
muhammadbiilal <cs21.bilal@gmail.com>
2024-10-20 16:27:25 +0500

    user login check splash screen

--- lib/firebase_services/splash_services.dart
+++ lib/firebase_services/splash_services.dart
@@ -1,16 +1,31 @@
 import 'dart:async';
 
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebaseflutter/ui/auth/login_screen.dart';
+import 'package:firebaseflutter/ui/posts/post_screen.dart';
 import 'package:flutter/material.dart';
 
 class SplashServices {
   void isLogin(BuildContext context) {
-    Timer(
-      const Duration(seconds: 1),
-      () => Navigator.push(
-        context,
-        MaterialPageRoute(builder: (context) => const LoginScreen()),
-      ),
-    );
+    final auth = FirebaseAuth.instance;
+    final user = auth.currentUser;
+
+    if (user != null) {
+      Timer(
+        const Duration(seconds: 1),
+        () => Navigator.push(
+          context,
+          MaterialPageRoute(builder: (context) => const PostScreen()),
+        ),
+      );
+    } else {
+      Timer(
+        const Duration(seconds: 1),
+        () => Navigator.push(
+          context,
+          MaterialPageRoute(builder: (context) => const LoginScreen()),
+        ),
+      );
+    }
   }
 }
--- lib/ui/posts/post_screen.dart
+++ lib/ui/posts/post_screen.dart
@@ -1,3 +1,4 @@
+import 'package:firebaseflutter/firebase_services/auth_service.dart';
 import 'package:flutter/material.dart';
 
 class PostScreen extends StatefulWidget {
@@ -14,6 +15,14 @@ class _PostScreenState extends State<PostScreen> {
       appBar: AppBar(
         automaticallyImplyLeading: false,
         title: const Text('Post Screen'),
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.logout_outlined),
+            onPressed: () async {
+              await AuthService().signOut(context: context);
+            },
+          ),
+        ],
       ),
     );
   }
