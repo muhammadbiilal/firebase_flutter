d52da0c7f47207e2a85188832df3c14e70d49b93
muhammadbiilal <cs21.bilal@gmail.com>
2024-10-29 00:44:18 +0500

    firease animated widget and the streaming to fetch data from db

--- lib/ui/posts/post_screen.dart
+++ lib/ui/posts/post_screen.dart
@@ -1,10 +1,12 @@
 // ignore_for_file: use_build_context_synchronously
 
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_database/ui/firebase_animated_list.dart';
 import 'package:firebaseflutter/ui/auth/login_screen.dart';
 import 'package:firebaseflutter/ui/posts/add_posts.dart';
 import 'package:firebaseflutter/utils/utils.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_database/firebase_database.dart';
 
 class PostScreen extends StatefulWidget {
   const PostScreen({super.key});
@@ -15,6 +17,8 @@ class PostScreen extends StatefulWidget {
 
 class _PostScreenState extends State<PostScreen> {
   final auth = FirebaseAuth.instance;
+  final ref = FirebaseDatabase.instance.ref('Post');
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -38,8 +42,44 @@ class _PostScreenState extends State<PostScreen> {
           ),
         ],
       ),
-      body: const Center(
-        child: Text('Post Screen'),
+      body: Column(
+        children: [
+          // FirebaseAnimatedList for data fetching and rendering
+          // Expanded(
+          // child: FirebaseAnimatedList(
+          // query: ref,
+          // defaultChild: const Text('Loading...'),
+          // itemBuilder: (context, snapshot, animation, index) {
+          // return ListTile(
+          // title: Text(snapshot.child('title').value.toString()),
+          // );
+          // },
+          // ),
+          // ),
+          // firebase with streambuilder for real-time updates
+          Expanded(
+            child: StreamBuilder(
+              stream: ref.onValue,
+              builder: (context, AsyncSnapshot<DatabaseEvent> snapshot) {
+                Map<dynamic, dynamic> map =
+                    snapshot.data!.snapshot.value as dynamic;
+                List<dynamic> list = [];
+                list.clear();
+                list = map.values.toList();
+                if (!snapshot.hasData) return const SizedBox.shrink();
+                return ListView.builder(
+                  itemCount: snapshot.data!.snapshot.children.length,
+                  itemBuilder: (context, index) {
+                    return ListTile(
+                      title: Text(list[index]['title'] as String),
+                      subtitle: Text(list[index]['id'] as String),
+                    );
+                  },
+                );
+              },
+            ),
+          ),
+        ],
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
